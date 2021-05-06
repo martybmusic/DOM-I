7608acfc34667e535814ef0bf93bb343c4f2e2fc
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-05-05 21:44:01 -0500

    Initial update

--- js/index.js
+++ js/index.js
@@ -40,3 +40,6 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const ctaPic = document.getElementById('cta-img');
+ctaPic.setAttribute('src', siteContent['cta']['img-src']);
